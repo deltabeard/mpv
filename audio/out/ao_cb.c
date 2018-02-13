b3953cc0666c4ad8608cf9542173a32f4d6c69a8
Mahyar Koshkouei <mahyar.koshkouei@gmail.com>
2018-02-13 15:57:46 +0000

    Cleanup
    Signed-off-by: Mahyar Koshkouei <mahyar.koshkouei@gmail.com>

--- audio/out/ao_cb.c
+++ audio/out/ao_cb.c
@@ -33,34 +33,30 @@
 #include "options/m_option.h"
 #include "osdep/timer.h"
 
-struct priv {
-	float buflen;
-};
-
 static struct ao *ao_cb = NULL;
 
 int audio_callback(uint16_t *stream, int len)
 {
     struct ao *ao = ao_cb;
 
-    if(ao == NULL)
+    if (ao == NULL) {
+		MP_ERR(ao, "audio callback was not initialised");
 		return -1;
+	}
 
-	if(stream == NULL)
-	{
+	if (stream == NULL) {
 		MP_ERR(ao, "stream must not be NULL");
 		return -2;
 	}
 
-    void *data[1] = {stream};
-
     if (len % ao->sstride)
-        MP_ERR(ao, "SDL audio callback not sample aligned");
+        MP_ERR(ao, "audio callback not sample aligned");
 
-    // Time this buffer will take, plus assume 1 period (1 callback invocation)
-    // fixed latency.
+    /* Time this buffer will take, plus assume 1 period (1 callback invocation)
+     * fixed latency.
+     */
     double delay = 2 * len / (double)ao->bps;
-
+    void *data[1] = {stream};
 	struct ao_convert_fmt conv = {
 		.src_fmt    = AF_FORMAT_S16,
 		.channels   = 2,
@@ -70,19 +66,12 @@ int audio_callback(uint16_t *stream, int len)
 
 	return ao_read_data_converted(ao, &conv, data, len / ao->sstride,
 			mp_time_us() + 1000000LL * delay);
-    //return ao_read_data(ao, data, len / ao->sstride, mp_time_us() + 1000000LL * delay);
 }
 
 static int init(struct ao *ao)
 {
-    struct priv *priv = ao->priv;
-
 	ao->format = AF_FORMAT_S16;
-
-    /* Force Stereo for now. TODO: Add Mono support. */
-    //if (!ao_chmap_sel_adjust(ao, &sel, &ao->channels))
 	mp_chmap_from_channels(&ao->channels, 2);
-
     ao->bps = ao->channels.num * ao->samplerate * af_fmt_to_bytes(ao->format);
 
 	MP_INFO(ao, "Samplerate: %d Hz Channels: %d Format: %s\n", ao->samplerate,
@@ -94,17 +83,14 @@ static int init(struct ao *ao)
     return 0;
 }
 
-// close audio device
 static void uninit(struct ao *ao)
 {
 	ao_cb = NULL;
-
 	return;
 }
 
 static void resume (struct ao *ao)
 {
-	// TODO
 	return;
 }
 
@@ -116,13 +102,8 @@ const struct ao_driver audio_out_audio_cb = {
     .init      = init,
 	.uninit    = uninit,
 	.resume    = resume,
-    .priv_size = sizeof(struct priv),
-    .priv_defaults = &(const struct priv) {
-        .buflen = 0, // use SDL default (TODO: Check if this is required)
-    },
-    .options = (const struct m_option[]) {
-        OPT_FLOAT("buflen", buflen, 0),
-        {0}
-    },
-    .options_prefix = "audio-cb",
+    .priv_size = 0,
+    .priv_defaults = NULL,
+    .options = NULL,
+    .options_prefix = NULL,
 };
